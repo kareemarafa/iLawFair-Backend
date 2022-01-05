fc16bf9a76b65a1869d9334140411ddabb4a8ce9
Kareem Arafa <kareem.arafa@afaqy.com>
2022-01-05 16:20:42 +0200

    initial commit

---
+++ apps/web-builder/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ionhour-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web-builder';
+}
