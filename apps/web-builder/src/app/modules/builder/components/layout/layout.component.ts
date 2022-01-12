55c56da1d5e7c39712ac0ef282194d2ceb58edeb
Mohamed Sami <mohamed.sami@intercom.com.eg>
2022-01-12 17:27:53 +0200

    update packages

--- apps/web-builder/src/app/modules/builder/components/layout/layout.component.ts
+++ apps/web-builder/src/app/modules/builder/components/layout/layout.component.ts
@@ -1,67 +1,51 @@
-import {
-  Component,
-  ComponentFactoryResolver,
-  ComponentRef,
-  ViewChild,
-  ViewContainerRef,
-} from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { ComponentItem } from '../../../../model';
-import { ElementsService } from '../../services';
-import {CdkDragDrop, moveItemInArray, CdkDrag} from '@angular/cdk/drag-drop';
+import { Component, ComponentFactoryResolver, ComponentRef, ViewChild, ViewContainerRef } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map, shareReplay } from 'rxjs/operators'
+import { ComponentItem } from '../../../../model'
+import { ElementsService } from '../../services'
+import { CdkDragDrop, moveItemInArray, CdkDrag } from '@angular/cdk/drag-drop'
 
 @Component({
   selector: 'ionhour-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss'],
+  styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
-  components: ComponentItem[] = [];
-  componentsRef: ComponentRef<any>[] = [];
+  components: ComponentItem[] = []
+  componentsRef: ComponentRef<any>[] = []
 
   @ViewChild('container', { read: ViewContainerRef })
-  container!: ViewContainerRef;
+  container!: ViewContainerRef
 
-  constructor(
-    private componentFactoryResolver: ComponentFactoryResolver,
-    private breakpointObserver: BreakpointObserver,
-    private elementsService: ElementsService
-  ) {
-    const preview = elementsService.previewElements$;
-    preview.subscribe((component) => this.add(component));
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private breakpointObserver: BreakpointObserver, private elementsService: ElementsService) {
+    const preview = elementsService.previewElements$
+    preview.subscribe((component) => this.add(component))
   }
 
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map((result) => result.matches),
+    shareReplay()
+  )
 
   add(component: any) {
-    this.container.clear();
+    this.container.clear()
     this.componentsRef = []
 
-    this.components.push({ component });
+    this.components.push({ component })
     for (const component of this.components) {
-      this.createComponent(component.component);
+      this.createComponent(component.component)
     }
   }
 
   createComponent(component: any): void {
-    const dynamicComponentFactory =
-      this.componentFactoryResolver.resolveComponentFactory(component);
-    const componentRef = this.container.createComponent(
-      dynamicComponentFactory
-    );
+    const dynamicComponentFactory = this.componentFactoryResolver.resolveComponentFactory(component)
+    const componentRef = this.container.createComponent(dynamicComponentFactory)
     this.componentsRef.push(componentRef)
   }
 
   drop(event: CdkDragDrop<any>) {
-    this.container.move(this.componentsRef[event.previousIndex].hostView, event.currentIndex);
-    moveItemInArray(this.componentsRef, event.previousIndex, event.currentIndex);
+    this.container.move(this.componentsRef[event.previousIndex].hostView, event.currentIndex)
+    moveItemInArray(this.componentsRef, event.previousIndex, event.currentIndex)
   }
-
 }
