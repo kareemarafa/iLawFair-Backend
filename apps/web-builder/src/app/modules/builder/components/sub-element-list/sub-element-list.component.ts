bc3439168cc1099d413bb0aef6e33be95cc7af60
Mohamed Sami <mohamed.sami@intercom.com.eg>
2022-01-13 23:50:25 +0200

    Add delete component option

--- apps/web-builder/src/app/modules/builder/components/sub-element-list/sub-element-list.component.ts
+++ apps/web-builder/src/app/modules/builder/components/sub-element-list/sub-element-list.component.ts
@@ -1,29 +1,27 @@
-import { Component } from '@angular/core';
-import { ElementsService } from '../../services';
-import { builderElements } from '@ionhour/ui';
-import { ComponentInterface, ModuleInterface } from '@ionhour/interfaces';
+import { Component } from '@angular/core'
+import { ElementsService } from '@ionhour/core'
+import { builderElements } from '@ionhour/ui'
+import { ComponentInterface, ModuleInterface } from '@ionhour/interfaces'
 
 @Component({
   selector: 'ionhour-sub-element-list',
   templateUrl: './sub-element-list.component.html',
-  styleUrls: ['./sub-element-list.component.scss'],
+  styleUrls: ['./sub-element-list.component.scss']
 })
 export class SubElementListComponent {
-  subComponents!: ComponentInterface[];
+  subComponents!: ComponentInterface[]
 
   constructor(public elementService: ElementsService) {
-    const items = elementService.currentElement$;
-    items.subscribe((key) => (this.subComponents = this.getSubComponents(key)));
+    const items = elementService.currentElement$
+    items.subscribe((key) => (this.subComponents = this.getSubComponents(key)))
   }
 
   getSubComponents(key: string): ComponentInterface[] {
-    const { components } = builderElements.filter(
-      (item: ModuleInterface) => item.moduleName === key
-    )[0];
-    return components;
+    const { components } = builderElements.filter((item: ModuleInterface) => item.moduleName === key)[0]
+    return components
   }
 
   pushComponent(componentClass: any): void {
-    this.elementService.pushToPreview(componentClass);
+    this.elementService.pushToPreview(componentClass)
   }
 }
