74ca2804bc4d84a65bcdf9a16be22999bfeeafa1
Kareem Arafa <kareem.arafa@afaqy.com>
2022-01-13 19:08:53 +0200

    fix toggle status on change module

--- apps/web-builder/src/app/modules/builder/components/element-list/element-list.component.ts
+++ apps/web-builder/src/app/modules/builder/components/element-list/element-list.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
-import { builderElements } from '@ionhour/ui';
-import { ElementsService } from '../../services';
-import { MatSlideToggleChange } from '@angular/material/slide-toggle/slide-toggle';
+import { Component, OnInit } from '@angular/core'
+import { builderElements } from '@ionhour/ui'
+import { ElementsService } from '../../services'
+import { MatSlideToggleChange } from '@angular/material/slide-toggle/slide-toggle'
+import { ModuleInterface } from '@ionhour/interfaces'
+import { lastValueFrom } from 'rxjs'
 
 @Component({
   selector: 'ionhour-element-list',
   templateUrl: './element-list.component.html',
-  styleUrls: ['./element-list.component.scss'],
+  styleUrls: ['./element-list.component.scss']
 })
-export class ElementListComponent {
-  builderElements = builderElements;
+export class ElementListComponent implements OnInit {
+  builderElements = builderElements
+  activeModule!: string
 
   constructor(private elementService: ElementsService) {}
 
   setCurrentElement(element: MatSlideToggleChange): void {
-    const name = element?.source?.name ?? '';
-    const status = element?.checked;
+    const name = element?.source?.name ?? ''
+    const status = element?.checked
     if (status) {
-      this.elementService.setCurrentElement(name);
+      this.elementService.setCurrentElement(name)
     }
+  }
+
+  ngOnInit() {
+    this.elementService.currentElement$.subscribe((module) => (this.activeModule = module))
+  }
+
+  getCheckStatus(element: ModuleInterface): boolean {
+    return this.activeModule === element.moduleName
   }
 }
