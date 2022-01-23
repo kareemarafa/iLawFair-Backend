223ee203e2ffc7ae9acebc997772dbde940a5a20
Kareem Arafa <kareem.arafa@afaqy.com>
2022-01-23 12:31:10 +0200

    enhance UI

--- libs/ui/src/lib/about/components/about-a/about-a.component.ts
+++ libs/ui/src/lib/about/components/about-a/about-a.component.ts
@@ -1,8 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ionhour-about-a',
   templateUrl: './about-a.component.html',
   styleUrls: ['./about-a.component.scss']
 })
-export class AboutAComponent {}
+export class AboutAComponent {
+  componentData = {
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqKetpqadnlRWmF4nl6bixjNNEpZXILvAj8g&usqp=CAU',
+    title: 'Lorem Ipsum',
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+  }
+}
