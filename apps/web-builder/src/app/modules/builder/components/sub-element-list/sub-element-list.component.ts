0651f68eb46c865e11ac0371f7566632fa7a31dc
Mohamed Sami <mohamed.sami@intercom.com.eg>
2022-01-24 01:27:11 +0200

    Edit sub-element flow && Show components in sub-element component

--- apps/web-builder/src/app/modules/builder/components/sub-element-list/sub-element-list.component.ts
+++ apps/web-builder/src/app/modules/builder/components/sub-element-list/sub-element-list.component.ts
@@ -1,28 +1,58 @@
-import { Component, Input } from '@angular/core'
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild, ViewContainerRef } from '@angular/core'
 import { ElementsService } from '@ionhour/core'
 import { builderElements } from '@ionhour/ui'
 import { IComponent, ModuleInterface } from '@ionhour/interfaces'
+import { map } from 'rxjs'
+import { WrapperComponent } from './wrapper/wrapper.component'
 
 @Component({
   selector: 'ionhour-sub-element-list',
   templateUrl: './sub-element-list.component.html',
   styleUrls: ['./sub-element-list.component.scss']
 })
-export class SubElementListComponent {
+export class SubElementListComponent implements AfterViewInit, OnChanges {
   subComponents!: IComponent[]
-  @Input() show!: boolean
+  @Input() moduleName!: string | null
 
-  constructor(public elementService: ElementsService) {
-    const items = elementService.currentElement$
-    items.subscribe((key) => (this.subComponents = key ? this.getSubComponents(key) : []))
+  @ViewChild('container', { read: ViewContainerRef }) container?: ViewContainerRef
+
+  constructor(public elementService: ElementsService) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    Promise.resolve().then((e) => {
+      if (this.moduleName) this.getSubComponents(this.moduleName)
+    })
   }
 
-  getSubComponents(key: string): IComponent[] {
-    const { components } = builderElements.filter((item: ModuleInterface) => item.moduleName === key)[0]
-    return components
+  ngAfterViewInit() {
+    Promise.resolve().then((e) => {
+      if (this.moduleName) this.getSubComponents(this.moduleName)
+    })
   }
 
-  pushComponent(componentClass: any): void {
-    this.elementService.pushToPreview(componentClass)
+  getSubComponents(moduleName: string) {
+    const { components } = builderElements.filter((item: ModuleInterface) => item.moduleName === moduleName)[0]
+    this.viewComponent(components)
+  }
+
+  viewComponent(components: IComponent[]) {
+    // Reset containerRef & componentsRef
+    this.container?.clear()
+
+    for (const component of components) {
+      this.createComponent(component.componentClass)
+    }
+  }
+
+  // createComponent(component: any): void {
+  //   this.container?.createComponent(component)
+  // }
+
+  createComponent(component: any): void {
+    const componentRef: any = this.container?.createComponent(WrapperComponent)
+    console.log(componentRef)
+    if (componentRef) {
+      componentRef.instance.component = component
+    }
   }
 }
