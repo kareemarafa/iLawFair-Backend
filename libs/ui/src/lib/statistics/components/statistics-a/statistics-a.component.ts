1c67f01abeadeac8fb4c1b0315a71dfc0db3f26e
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-04 23:27:15 +0200

    Merge branch 'develop' of https://github.com/kareemarafa/ionhour-web-me into services
    # Conflicts:
    #	apps/web-builder/src/styles.scss

---
+++ libs/ui/src/lib/statistics/components/statistics-a/statistics-a.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'ionhour-statistics-a',
+  templateUrl: './statistics-a.component.html',
+  styleUrls: ['./statistics-a.component.scss']
+})
+export class StatisticsAComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
