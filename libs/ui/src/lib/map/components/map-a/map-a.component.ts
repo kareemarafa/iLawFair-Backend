a0788bce5219ef698663658ef65c7a15a8bb0d7a
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-06 03:08:01 +0200

    Merge branch 'develop' of https://github.com/kareemarafa/ionhour-web-me into prices
    # Conflicts:
    #	apps/web-builder/src/app/modules/builder/builder.module.ts

--- libs/ui/src/lib/map/components/map-a/map-a.component.ts
+++ libs/ui/src/lib/map/components/map-a/map-a.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core'
-
-declare let google: { maps: { Map: new (arg0: HTMLElement | null, arg1: { center: { lat: number; lng: number }; zoom: number }) => any } }
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ionhour-map-a',
   templateUrl: './map-a.component.html',
   styleUrls: ['./map-a.component.scss']
 })
-export class MapAComponent implements OnInit {
-  map: any
-
-  ngOnInit(): void {
-    this.map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8
-    })
-  }
-}
+export class MapAComponent {}
