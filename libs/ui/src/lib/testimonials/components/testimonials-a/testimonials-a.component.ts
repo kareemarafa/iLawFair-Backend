999a0136f880bff1436cbd1fa94d2aef04000e42
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-06 03:01:16 +0200

    Merge branch 'develop' of https://github.com/kareemarafa/ionhour-web-me into prices
    # Conflicts:
    #	apps/web-builder/src/app/modules/builder/builder.module.ts

--- libs/ui/src/lib/testimonials/components/testimonials-a/testimonials-a.component.ts
+++ libs/ui/src/lib/testimonials/components/testimonials-a/testimonials-a.component.ts
@@ -1,12 +1,37 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'ionhour-testimonials-a',
   templateUrl: './testimonials-a.component.html',
   styleUrls: ['./testimonials-a.component.scss']
 })
-export class TestimonialsAComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TestimonialsAComponent {
+  componentData = {
+    title: 'Testimonials',
+    description:
+      'Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.',
+    items: [
+      {
+        imageUrl: './assets/500x300.png',
+        content: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+        name: 'Mohamed Hassan',
+        title: 'CEO',
+        companyName: 'Dot Us'
+      },
+      {
+        imageUrl: './assets/500x300.png',
+        content: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+        name: 'Mohamed Hassan',
+        title: 'CEO',
+        companyName: 'Dot Us'
+      },
+      {
+        imageUrl: './assets/500x300.png',
+        content: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+        name: 'Mohamed Hassan',
+        title: 'CEO',
+        companyName: 'Dot Us'
+      }
+    ]
+  }
 }
