50ca9e4f494db2ae77bac769cb78feebf9bc117d
Mohamed Khaled <flavasava1212@gmail.com>
2022-02-14 00:12:47 +0200

    create users module

--- apps/api-builder/src/app/modules/index.ts
+++ apps/api-builder/src/app/modules/index.ts
@@ -1,3 +1,4 @@
 import { ComponentsModule } from './components/components.module'
+import { UsersModule } from './users/users.module'
 
-export const features = [ComponentsModule]
+export const features = [ComponentsModule, UsersModule]
