7890ac556bbeb28fb97fbdb443eacda3da494617
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-23 23:24:40 +0200

    projects list

--- apps/web-builder/src/app/modules/projects/projects.module.ts
+++ apps/web-builder/src/app/modules/projects/projects.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ProjectsListComponent, ProjectsFormComponent } from './components'
 import { Route, RouterModule } from '@angular/router'
+import { MatTableModule } from '@angular/material/table'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
 const routes: Route[] = [
   {
@@ -16,6 +19,6 @@ const routes: Route[] = [
 
 @NgModule({
   declarations: [ProjectsListComponent, ProjectsFormComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  imports: [CommonModule, RouterModule.forChild(routes), MatTableModule, MatButtonModule, MatIconModule]
 })
 export class ProjectsModule {}
