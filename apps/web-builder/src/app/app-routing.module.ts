cc4a106b7426cfeee1ec456f3b872cf9517dc714
Kareem Arafa <kareem.arafa@afaqy.com>
2022-02-23 22:45:20 +0200

    auth UI

--- apps/web-builder/src/app/app-routing.module.ts
+++ apps/web-builder/src/app/app-routing.module.ts
@@ -4,14 +4,21 @@ import { RouterModule, Routes } from '@angular/router'
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then((m) => m.AuthModule)
+  },
+  {
+    path: 'builder',
     loadChildren: () => import('./modules/builder/builder.module').then((m) => m.BuilderModule)
   }
 ]
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
---
+++ apps/web-builder/src/app/modules/auth/components/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+
+@Component({
+  selector: 'ionhour-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  form: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder,
+    // private resumeAuthService: ResumeAuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.form = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    })
+  }
+
+  ngOnInit() {
+    // this.form.controls["username"].setValue("owner");
+    // this.form.controls["password"].setValue("123456789");
+  }
+
+  login() {
+    // const payload: LoginPayloadInterface = {
+    //   username: this.form.value.username,
+    //   password: this.form.value.password,
+    //   remember: false
+    // };
+    // this.store.dispatch({ type: ResumeAuthActionTypes.ResumeAPI_USER_LOGIN, payload })
+    // this.loginState.login(payload)
+    //   .pipe(
+    //     untilDestroyed(this)
+    //   )
+    //   .subscribe(() =>
+    return this.router.navigate(['builder'])
+    // );
+  }
+
+  ngOnDestroy() {
+    console.log('LoginComponent Destroyed')
+  }
+}
