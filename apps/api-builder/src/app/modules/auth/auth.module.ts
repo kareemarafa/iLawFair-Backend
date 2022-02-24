6088f86e96cbab6c3dbc7d711da7a042b6099bd9
Mohamed Khaled <flavasava1212@gmail.com>
2022-02-24 20:06:01 +0200

    - inject encryption service from encryption module and use it - delete the constants file from the app - inject config service and use secret from it

--- apps/api-builder/src/app/modules/auth/auth.module.ts
+++ apps/api-builder/src/app/modules/auth/auth.module.ts
@@ -4,17 +4,25 @@ import { UsersModule } from '../users/users.module'
 import { PassportModule } from '@nestjs/passport'
 import { LocalStrategy } from './strategys'
 import { JwtModule } from '@nestjs/jwt'
-import { jwtConstants } from './constants'
 import { JwtStrategy } from './strategys'
 import { AuthController } from './auth.controller'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { EncryptionModule } from '@ionhour/encryption'
 
 @Module({
   imports: [
+    EncryptionModule,
     forwardRef(() => UsersModule),
     PassportModule.register({ defaultStrategy: 'local' }),
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' }
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('jwtSecret'),
+        signOptions: {
+          expiresIn: configService.get<string>('jwtExpiresIn')
+        }
+      }),
+      inject: [ConfigService]
     })
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
--- apps/api-builder/src/app/modules/auth/auth.service.ts
+++ apps/api-builder/src/app/modules/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { TokenPayloadInterface } from './interfaces'
 import { JwtService } from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt'
 import { User } from '../users/users.entity'
+import { EncryptionService } from '../../../../../../libs/encryption/src/lib/encryption.service'
 
 @Injectable()
 export class AuthService {
@@ -11,16 +12,15 @@ export class AuthService {
     @Inject(forwardRef(() => UsersService))
     private usersService: UsersService,
     private jwtService: JwtService,
-    ) {}
+    private encryptionService: EncryptionService
+  ) {}
 
-  async validateUser(email: string, password: string): Promise<User> {
+  async validateUser(email: string, password: string): Promise<any> {
     const user: User = await this.usersService.findOneByEmail(email)
-    if (user) {
-      const isPasswordCorrect = await bcrypt.compare(password, user?.password)
-      if (isPasswordCorrect) {
-        delete user.password
-        return user
-      }
+    const isPasswordCorrect = user && (await this.encryptionService.compare(password, user.password))
+    if (user && isPasswordCorrect) {
+      const { password, ...result } = user
+      return result
     }
     return null
   }
@@ -36,7 +36,6 @@ export class AuthService {
       accessToken: this.jwtService.sign(payload)
     }
   }
-
 
   async register(user: User): Promise<User> {
     const _user: User = await this.usersService.findOneByEmail(user?.email)
