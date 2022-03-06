37554fe6145f0a5710d3945779860ff4d34830d1
Kareem Arafa <kareem.arafa@afaqy.com>
2022-03-06 19:13:22 +0200

    footer

---
+++ libs/ui/src/lib/footer/footer.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { FooterAComponent } from './components/'
+
+@NgModule({
+  declarations: [FooterAComponent],
+  imports: [CommonModule]
+})
+export class FooterModule {}
