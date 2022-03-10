02c72babf487175db8fd122fb610cb0db1839cf1
Kareem Arafa <kareem.arafa@afaqy.com>
2022-03-10 23:55:46 +0200

    count-a component dynamic form content

--- libs/ui/src/lib/count/components/count-a/count-a.component.ts
+++ libs/ui/src/lib/count/components/count-a/count-a.component.ts
@@ -3,16 +3,6 @@ import { ElementsService } from '@ionhour/core'
 import { takeWhile } from 'rxjs'
 import { CountdownConfig } from 'ngx-countdown'
 
-const CountdownTimeUnits: Array<[string, number]> = [
-  ['Y', 1000 * 60 * 60 * 24 * 365], // years
-  ['M', 1000 * 60 * 60 * 24 * 30], // months
-  ['D', 1000 * 60 * 60 * 24], // days
-  ['H', 1000 * 60 * 60], // hours
-  ['m', 1000 * 60], // minutes
-  ['s', 1000], // seconds
-  ['S', 1] // million seconds
-]
-
 @Component({
   selector: 'ionhour-count-a',
   templateUrl: './count-a.component.html',
