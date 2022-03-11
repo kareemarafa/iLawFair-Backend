d97835119122c5aab462e07ad8abb8a387b35410
Kareem Arafa <kareem.arafa@afaqy.com>
2022-03-11 04:56:51 +0200

    enhance upload folder

--- apps/api-builder/src/app/modules/media/media.controller.ts
+++ apps/api-builder/src/app/modules/media/media.controller.ts
@@ -9,6 +9,7 @@ import { editFileName, imageFileFilter } from '@ionhour/backend-core'
 import { AuthGuard } from '@nestjs/passport'
 import { AuthService } from '../auth/auth.service'
 import { UsersService } from '../users/users.service'
+import { join } from 'path'
 
 @Crud({
   model: {
@@ -31,7 +32,7 @@ export class MediaController implements CrudController<Media> {
   @Post('upload')
   @UseInterceptors(
     FileInterceptor('image', {
-      storage: diskStorage({ destination: './public/uploads', filename: editFileName }),
+      storage: diskStorage({ destination: join(__dirname, '../../..', 'public/uploads'), filename: editFileName }),
       fileFilter: imageFileFilter
     })
   )
