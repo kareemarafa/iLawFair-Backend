9d67326430a2fcbb4672be056affcc09d51a28ec
ALY Ahmed <G526337@mph.morpho.com>
2022-07-05 10:42:23 +0200

    change style sidebar

--- apps/web-builder/src/app/modules/shell/header/header.component.ts
+++ apps/web-builder/src/app/modules/shell/header/header.component.ts
@@ -1,65 +1,13 @@
 import { Component, OnInit } from '@angular/core'
-import { I18nService } from '../../../common/i18n.service'
-import {
-  ArcElement,
-  BarController,
-  BarElement,
-  CategoryScale,
-  Chart,
-  DoughnutController,
-  LinearScale,
-  LineController,
-  LineElement,
-  PieController,
-  PointElement,
-  PolarAreaController,
-  RadarController,
-  RadialLinearScale,
-  Title,
-  Tooltip,
-  Legend
-} from 'chart.js'
+
 @Component({
   selector: 'ionhour-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  constructor(private i18nService: I18nService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    Chart.register(
-      ArcElement,
-      BarController,
-      BarElement,
-      CategoryScale,
-      DoughnutController,
-      LinearScale,
-      LineController,
-      LineElement,
-      PieController,
-      PointElement,
-      PolarAreaController,
-      RadarController,
-      RadialLinearScale,
-      Title,
-      Tooltip,
-      Legend
-    )
-  }
-  get currentLang(): string {
-    return this.i18nService.language
-  }
-
-  get languages(): string[] {
-    return this.i18nService.supportedLanguages
-  }
-
-  setLanguage(language: string) {
-    this.i18nService.language = language
-    // const dir = language === 'en-US' ? 'ltr' : 'rtl';
-    // document.body.setAttribute('dir', dir);
-    // window.location.reload()
-  }
+  ngOnInit(): void {}
 }
 
