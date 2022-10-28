85a0979af55417ff69e32a6b714899992a65f5c2
Kareem Arafa <kareem@Kareems-MacBook-Pro.local>
2022-10-28 22:56:06 +0200

    fix: implement auth guard to meta and categories

--- apps/api-builder/src/app/modules/categories/categories.controller.ts
+++ apps/api-builder/src/app/modules/categories/categories.controller.ts
@@ -1,8 +1,9 @@
 import { Crud, CrudController } from '@nestjsx/crud'
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
-import { Controller } from '@nestjs/common'
+import {Controller, UseGuards} from '@nestjs/common'
 import {Category} from "./categories.entity";
 import {CategoriesService} from "./categories.service";
+import {AuthGuard} from "@nestjs/passport";
 
 @Crud({
   model: {
@@ -12,6 +13,7 @@ import {CategoriesService} from "./categories.service";
 @ApiBearerAuth()
 @Controller('categories')
 @ApiTags('categories')
+@UseGuards(AuthGuard('jwt'))
 export class CategoriesController implements CrudController<Category> {
   constructor(public service: CategoriesService) {}
 }
--- apps/api-builder/src/app/modules/meta/meta.controller.ts
+++ apps/api-builder/src/app/modules/meta/meta.controller.ts
@@ -1,8 +1,9 @@
 import { Crud, CrudController } from '@nestjsx/crud'
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
-import { Controller } from '@nestjs/common'
+import {Controller, UseGuards} from '@nestjs/common'
 import {Meta} from "./meta.entity";
 import {MetaService} from "./meta.service";
+import {AuthGuard} from "@nestjs/passport";
 
 @Crud({
   model: {
@@ -12,6 +13,7 @@ import {MetaService} from "./meta.service";
 @ApiBearerAuth()
 @Controller('meta')
 @ApiTags('meta')
+@UseGuards(AuthGuard('jwt'))
 export class MetaController implements CrudController<Meta> {
   constructor(public service: MetaService) {}
 }
