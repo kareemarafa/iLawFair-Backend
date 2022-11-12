807997d8cf002ad82956adbe465238895d1b8c37
Kareem <k4ho@live.com>
2022-11-12 17:17:56 +0200

    fix: export media and auth modules from indexed file to app module registry import

--- apps/api-admin/src/app/modules/index.ts
+++ apps/api-admin/src/app/modules/index.ts
@@ -2,5 +2,7 @@ import { AdminUsersModule } from './admin-users/admin-users.module'
 import {AdminMetaModule} from "./admin-meta/admin-meta.module";
 import {AdminCategoriesModule} from "./admin-categories/admin-categories.module";
 import {AdminTemplateModule} from "./admin-templates/admin-template.module";
+import {AdminAuthModule} from "./admin-auth/admin-auth.module";
+import {AdminMediaModule} from "./admin-media/admin-media.module";
 
-export const features = [AdminUsersModule,  AdminMetaModule, AdminCategoriesModule, AdminTemplateModule]
+export const features = [AdminAuthModule, AdminMediaModule, AdminUsersModule,  AdminMetaModule, AdminCategoriesModule, AdminTemplateModule]
