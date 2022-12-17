55311490a96c7877030f81d569ef744a208b02c9
Mohamed Khaled <flavasava1212@gmail.com>
2022-12-17 17:27:33 +0200

    Update profile The user can update his details and the password is mandatory for any update request

--- apps/tenant/src/app/modules/tenant-users/tenant-users.service.ts
+++ apps/tenant/src/app/modules/tenant-users/tenant-users.service.ts
@@ -1,8 +1,9 @@
-import {Injectable} from '@nestjs/common'
+import {BadRequestException, HttpStatus, Injectable} from '@nestjs/common'
 import {InjectRepository} from '@nestjs/typeorm'
 import {TenantUser} from './tenant-users.entity'
-import {Repository} from 'typeorm'
+import {Repository, UpdateResult} from 'typeorm'
 import {KamService} from "@ionhour/backend-core";
+import {AuthService} from "../tenant-auth/tenant-auth.service";
 
 @Injectable()
 export class TenantUsersService extends KamService<TenantUser> {
@@ -17,4 +18,26 @@ export class TenantUsersService extends KamService<TenantUser> {
   async findOneByEmail(email: string): Promise<TenantUser> {
     return this.repo.findOne({where: {email}})
   }
+
+  /**
+   * Updates a user
+   */
+  async update(
+    id: number,
+    userData: any,
+  ): Promise<UpdateResult> {
+    const existUser: TenantUser = await this.findOneByEmail(userData.email);
+    if (
+      existUser &&
+      id !== existUser.id &&
+      userData.email === existUser.email
+    ) {
+      throw new BadRequestException({
+        statusCode: HttpStatus.BAD_REQUEST
+      })
+    } else {
+      return this.repo.update(id, userData);
+    }
+  }
+
 }
